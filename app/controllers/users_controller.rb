32fcf9be263910859d9c2dde4d5862c3323a76c4
sql-fan <166389859+sql-fan@users.noreply.github.com>
2025-02-17 00:22:51 +0200

    small fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
-    @events = @user.events.all
+    @events = @user.events_created.all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_16_191727) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_events_on_user_id"
+    t.integer "creator_id", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,5 +32,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_16_191727) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "events", "users"
+  add_foreign_key "events", "users", column: "creator_id"
 end
