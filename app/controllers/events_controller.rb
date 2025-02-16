f1b1ebfe01b6f3fd11e76d36c7d0f629138d6772
sql-fan <166389859+sql-fan@users.noreply.github.com>
2025-02-17 00:50:19 +0200

    add new event page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,27 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: %i[ show  ]
   def index
-    
+    @events = Event.all
   end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "users/:id", to: "users#show", as: "user"
+  resources :users
+  resources :events
+  # get "users/:id", to: "users#show", as: "user"
+  # get "users/:id", to: "users#show", as: "user"
 
   root "events#index"
 end
