2a7fdff0f24553c8a8f4e7e3a92b93d0f12d8bb9
sql-fan <166389859+sql-fan@users.noreply.github.com>
2025-02-20 15:34:03 +0200

    add joined events to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @events = @user.events_created.all
+    @events_joined = @user.attended_events.all
   end
 end
